1db516c654f1c0eef021061ce9e07f9f78f3da19
Luke <lukechesleyfive@gmail.com>
2023-04-07 10:37:33 -0400

    modified:   Gemfile 	modified:   Gemfile.lock 	new file:   MLB_game_data_2023.ipynb 	modified:   app/assets/stylesheets/application.css 	modified:   app/controllers/articles_controller.rb 	modified:   app/javascript/application.js 	new file:   app/views/articles/_form.html.erb 	new file:   app/views/articles/edit.html.erb 	modified:   app/views/articles/index.html.erb 	modified:   app/views/articles/new.html.erb 	modified:   app/views/articles/show.html.erb 	modified:   app/views/layouts/application.html.erb 	modified:   config/routes.rb 	modified:   getting_mlb_data.rb 	new file:   mlb_game_data_2023.csv

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"
 
+  delete '/articles', to: 'articles#destroy_all', as: 'delete_all_articles'
+
   resources :articles
 end
--- getting_mlb_data.rb
+++ getting_mlb_data.rb
@@ -1,5 +1,17 @@
+# run ipybn file to update csv then run this in ruby termial -> rails console
+
+
 require 'csv'
 
-CSV.foreach("/home/luke/mlb_game_data_2023.csv", headers: true, col_sep: ",") do |row|
-    p row
+data = CSV.read("/home/luke/projects/blog/mlb_game_data_2023.csv", headers: true, col_sep: ",")
+
+data.each do |row|
+    puts row.values_at("home_team","away_team")
+
 end
+
+
+# data.each do |row|
+#   article = Article.new(title:row.values_at("date","home_team","away_team"), body: row.values_at("home_team","home_runs","away_team","away_runs"))
+#   article.save
+# end
