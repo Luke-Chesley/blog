86ef55e52b5d26d6ebcd6a78f864f2c1516d2f37
Luke <lukechesleyfive@gmail.com>
2023-04-10 11:11:34 -0400

    modified:   MLB_game_data_2023.ipynb 	new file:   app/assets/images/Major_League_Baseball_logo.svg.webp 	modified:   app/assets/stylesheets/application.css 	modified:   app/models/article.rb 	new file:   app/models/user.rb 	modified:   app/views/articles/index.html.erb 	modified:   app/views/articles/show.html.erb 	new file:   db/migrate/20230409172344_create_users.rb 	new file:   db/migrate/20230409195658_add_image_url_to_articles.rb 	modified:   db/schema.rb 	modified:   getting_mlb_data.rb 	new file:   test/fixtures/users.yml 	new file:   test/models/user_test.rb

--- getting_mlb_data.rb
+++ getting_mlb_data.rb
@@ -1,17 +1,12 @@
-# run ipybn file to update csv then run this in ruby termial -> rails console
+# run ipybn file to update csv then run this in rails console
 
 
-require 'csv'
+# require 'csv'
 
-data = CSV.read("/home/luke/projects/blog/mlb_game_data_2023.csv", headers: true, col_sep: ",")
-
-data.each do |row|
-    puts row.values_at("home_team","away_team")
-
-end
+# data = CSV.read("/home/luke/projects/blog/mlb_game_data_2023.csv", headers: true, col_sep: ",")
 
 
 # data.each do |row|
-#   article = Article.new(title:row.values_at("date","home_team","away_team"), body: row.values_at("home_team","home_runs","away_team","away_runs"))
+#   article = Article.new(title:row.values_at("date","home_team","away_team"), body: row.values_at("home_team","home_runs","away_team","away_runs"),image_url: "https://upload.wikimedia.org/wikipedia/commons/4/4d/Cat_March_2010-1.jpg")
 #   article.save
 # end
