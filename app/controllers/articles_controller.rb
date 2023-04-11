cd79a6c9e4dc21c135958080c8de47ac563955f5
Luke <lukechesleyfive@gmail.com>
2023-04-11 16:57:12 -0400

    modified:   app/assets/stylesheets/application.css 	modified:   app/controllers/articles_controller.rb 	modified:   app/models/article.rb 	modified:   app/views/articles/show.html.erb 	modified:   db/migrate/20230403234721_create_articles.rb 	deleted:    db/migrate/20230409195658_add_image_url_to_articles.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -51,6 +51,6 @@ class ArticlesController < ApplicationController
 
   private
     def article_params
-      params.require(:article).permit(:title, :body)
+      params.require(:article).permit(:title, :body, :image_url)
     end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
-    validates :title, presence: true
-    validates :body, presence: true, length: { minimum: 5 }
-    attr_accessor :image_url, :string
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 5 }
+    
   end
   
