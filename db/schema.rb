62332b449f016ce7147f27252bb06775a01500a3
Luke <lukechesleyfive@gmail.com>
2023-04-18 20:22:18 -0400

    modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -19,11 +19,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_09_195658) do
     t.string "image_url"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
